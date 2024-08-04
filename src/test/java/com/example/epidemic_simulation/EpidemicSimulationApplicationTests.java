0f807a933d12343b7c4412603e362bffdbcd8c0c
AleksanderPostrzednik <antaroupost@gmail.com>
2024-08-04 19:19:21 +0200

    Initial commit with Spring Boot project

---
+++ src/test/java/com/example/epidemic_simulation/EpidemicSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epidemic_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
